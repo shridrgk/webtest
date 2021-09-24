d5dd1325d5c2b3817e63a0e287ef9ea6897099cb
SHRIDHARA R <shridhara055@gmail.com>
2021-09-24 22:59:01 +0530

    commit

--- src/test/java/com/example/testing/Test1.java
+++ src/test/java/com/example/testing/Test1.java
@@ -4,16 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-
-
 public class Test1 {
-    
+
     @Test
-public void chromdemo(){
-		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+    public void chromdemo() {
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
         driver.manage().window().maximize();
-}
+        System.out.println(driver.getTitle());
+    }
 
 }
